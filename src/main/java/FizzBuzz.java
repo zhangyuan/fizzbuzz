622592ac6d8129ac3a7e3b0ed5f7c623f8b8f262
Yuan <zhangyuan@users.noreply.github.com>
2017-06-02 12:05:21 +0800

    finish fizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,29 @@
 public class FizzBuzz {
+    private int from;
+    private int to;
+
+    public FizzBuzz(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public String[] perform() {
+        String[] result = new String[this.to];
+
+        for (int i = 0; i < this.to; i ++) {
+            int number = i + 1;
+            if (number % 3 == 0 && number % 5 == 0) {
+                result[i] = "FizzBuzz";
+            } else if (number % 3 == 0) {
+                result[i] = "Fizz";
+            } else if ( number % 5 == 0) {
+                result[i] = "Buzz";
+            }
+            else {
+                result[i] = String.valueOf(number);
+            }
+
+        }
+        return result;
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,3 +1,35 @@
-public class FizzBuzzTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_1_given_1_and_1() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz(1, 1);
+        assertArrayEquals(new String[]{"1"}, fizzBuzz.perform());
+    }
+
+    @Test
+    public void should_return_Fizz_given_1_and_3() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz(1, 3);
+        assertArrayEquals(new String[]{"1", "2", "Fizz"}, fizzBuzz.perform());
+    }
+
+    @Test
+    public void should_return_Buzz_given_1_and_3() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz(1, 5);
+        assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz"}, fizzBuzz.perform());
+    }
+
+    @Test
+    public void should_return_FizzBuzz_given_1_and_15() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz(1, 15);
+        assertArrayEquals(new String[]{
+                "1", "2", "Fizz",
+                "4", "Buzz", "Fizz",
+                "7", "8", "Fizz",
+                "Buzz", "11", "Fizz",
+                "13", "14", "FizzBuzz"
+        }, fizzBuzz.perform());
+    }
 }
